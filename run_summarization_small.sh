f0ae99d0fa16038a87739941f8bb55ced8f79c45
stevenjlyao <stevenjlyao@tencent.com>
2017-07-14 09:16:22 +0800

    uptodate

--- run_summarization_small.sh
+++ run_summarization_small.sh
@@ -1,5 +1,5 @@
 export PYTHONIOENCODING=UTF-8
-export DATA_PATH="$(pwd)/data/sum_data"
+export DATA_PATH="$(pwd)/data/giga_small"
 
 export VOCAB_SOURCE=${DATA_PATH}/vocab.bpe.32000
 export VOCAB_TARGET=${DATA_PATH}/vocab.bpe.32000
@@ -17,8 +17,8 @@ mkdir -p $MODEL_DIR
 
 python -m bin.train \
   --config_paths="
-      ./example_configs/conv_seq2seq.yml,
-      ./example_configs/train_seq2seq.yml,
+      ./example_configs/conv_seq2seq_sum.yml,
+      ./example_configs/train_seq2seq_sum.yml,
       ./example_configs/text_metrics_bpe.yml" \
   --model_params "
       vocab_source: $VOCAB_SOURCE
@@ -37,7 +37,7 @@ python -m bin.train \
         - $DEV_SOURCES
        target_files:
         - $DEV_TARGETS" \
-  --batch_size 32 \
+  --batch_size 128 \
   --eval_every_n_steps 5000 \
   --train_steps $TRAIN_STEPS \
   --output_dir $MODEL_DIR
